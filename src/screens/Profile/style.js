b7dbfc137e7aef8d497ba4b826e7dba87a49f606
Marinamorais <marinamorais@aluno.senai.br>
2024-02-26 17:10:06 -0300

    new commit

---
+++ src/screens/Profile/style.js
@@ -0,0 +1,19 @@
+import { StylesSheet } from "react-native";
+
+const styles = StylesSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#f5fcff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  navButton: {
+    marginTop: 20,
+    padding: 10,
+    backgroundColor: "lightblue",
+    color: "black",
+    borderRadius: 5,
+  },
+});
+
+export default styles;
